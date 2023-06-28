fbf1468500a3f53916dadef50b9d23bbd538aa14
Sean Bruen <kyau@kyau.net>
2023-06-27 21:17:32 -0400

    ignore: here be dragons

---
+++ src/app.js
@@ -0,0 +1,41 @@
+/**
+ * $KYAULabs: app.js,v 0.1.0 2023/06/27 17:07:41 kyau Exp $
+ * ▄▄▄▄ ▄▄▄▄ ▄▄▄▄▄▄▄▄▄ ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
+ * █ ▄▄ ▄ ▄▄ ▄ ▄▄▄▄ ▄▄ ▄    ▄▄   ▄▄▄▄ ▄▄▄▄  ▄▄▄ ▀
+ * █ ██ █ ██ █ ██ █ ██ █    ██   ██ █ ██ █ ██▀  █
+ * ■ ██▄▀ ██▄█ ██▄█ ██ █ ▀▀ ██   ██▄█ ██▄▀ ▀██▄ ■
+ * █ ██ █ ▄▄ █ ██ █ ██ █    ██▄▄ ██ █ ██ █  ▄██ █
+ * ▄ ▀▀ ▀ ▀▀▀▀ ▀▀ ▀ ▀▀▀▀    ▀▀▀▀ ▀▀ ▀ ▀▀▀▀ ▀▀▀  █
+ * ▀▀▀▀▀▀▀▀▀▀▀▀▀▀ ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀ ▀▀▀▀▀▀▀▀▀▀▀▀▀
+ *
+ * Hexforged - RESTful API Server
+ * Copyright (C) 2023 KYAU Labs (https://kyaulabs.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+const express = require('express');
+const app = express();
+
+app.disable('x-powered-by');
+
+app.use((req, res, next) => {
+    res.setHeader('X-Powered-By', 'hexforged-api');
+    res.status(200).json({
+        message: 'It works!'
+    });
+    next();
+});
+
+module.exports = app;
